d17ab3a508c275891c20042b91d2bd7ac1928059
Mohammed Yahya Hussain <yahyahussain9704@gmail.com>
2026-05-27 01:53:01 +0530

    add app and CI/CD pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,43 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "autoops_sg" {
+  name        = "autoops-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "autoops_server" {
-  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 (us-east-1)
-  instance_type = "t3.micro"               # Free tier!
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t3.micro"
+  key_name               = "autoops-key"
+  vpc_security_group_ids = [aws_security_group.autoops_sg.id]
 
   tags = {
     Name = "autoops-server"
   }
+}
+
+output "server_ip" {
+  value = aws_instance.autoops_server.public_ip
 }
